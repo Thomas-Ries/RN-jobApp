0ea73f3df8b964e759d838b689803dc6375bf16a
thomas <thomas.ries-balsamo@epitech.eu>
2023-06-23 16:43:03 +0200

    feat(popular job): add first UI layer / WIP fetch API data)

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,12 +1,49 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { useState } from 'react'
+import { View, Text, TouchableOpacity, FlatList, ActivityIndicator } from 'react-native';
+import { useRouter } from 'expo-router';
 
-import styles from './popularjobs.style'
+import styles from './popularjobs.style';
+import { COLORS, SIZES } from '../../../constants';
+import PopularJobCard from '../../common/cards/popular/PopularJobCard';
 
 const Popularjobs = () => {
+  const router = useRouter();
+  const isLoading = false;
+  const error = false;
+
   return (
-    <View>
-      <Text>Popularjobs</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>
+          Popularjobs
+        </Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Afficher tout</Text>
+        </TouchableOpacity>
+      </View>
+
+    <View
+    style={styles.container}>
+      {isLoading ? (
+        <ActivityIndicator size="large" colors={COLORS.primary} />
+      ) : error ? (
+        <Text> OOPS ! Quelque chose s'est mal passé 🧐 </Text>
+      ): (
+        <FlatList
+        data={[1, 2, 3, 4, 5, 6, 7, 8, 9]}
+        keyExtractor={item => item?.job_id}
+        contentContainerStyle={{ columnGap: SIZES.medium }}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        renderItem={({ item }) => (
+          <PopularJobCard
+          item={item} />
+        )}>
+
+        </FlatList>
+      )}
+    </View>
+
     </View>
   )
 }
